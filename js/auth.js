70c324a3ff0baae852777ac15e3687c466de5d90
jbalisi <jeggobalisi61399@gmail.com>
2019-04-20 21:45:49 -0400

    added simple login mechanism

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,43 @@
-window.alert("Hello");
+var count=2;
+
+function validate(){
+
+  var user = document.login.username.value;
+  var pass = document.login.password.value;
+  var valid = false;
+  var usernameArray = ["admin", "Monty"];
+  var passwordArray = ["420", "321"];
+
+  for(var i = 0; i < usernameArray.length; i++)
+{
+
+  if((user == usernameArray[i]) && (pass == passwordArray[i]))
+  {
+    valid = true;
+    break;
+  }
+
+}
+  if(valid){
+    alert("Login was successful");
+    window.location = "profile.html";
+    return false;
+  }
+  var again = " tries";
+  if (count == 1)
+  {
+    again = " try";
+  }
+  if (count >= 1)
+  {
+      alert("Wrong password or username");
+      count--;
+  }
+
+  else{
+
+  }
+
+
+
+}
